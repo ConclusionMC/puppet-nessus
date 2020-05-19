7a6bf340ecfca8a13b4bba935e904fba3650ef6f
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2020-05-19 16:09:36 +0200

    Fix build pipeline, puppet6 and new ruby

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,8 @@
 # @param activation_code The nessus code used to activate home or professional feeds.
 # @param package_ensure Ensure the package is installed, latest, or absent.
 # @param package_name The name of the package to install
+# @param proxy_host Is the nessus behind a http_proxy
+# @param proxy_port On what port is the http_proxy reachable
 # @param service_ensure State if the service should be running or stopped.
 # @param service_name The name of the nessus service.
 # @param service_enable Should the service be enabled upon boot
@@ -34,15 +36,15 @@
 # Copyright 2014 Adam Crews, unless otherwise noted.
 #
 class nessus (
-  Optional[String[1]]        $activation_code = undef,
-  String[1]                  $package_ensure  = $nessus::params::package_ensure,
-  String[1]                  $package_name    = $nessus::params::package_name,
+  Optional[String[1]]                  $activation_code = undef,
+  String[1]                            $package_ensure  = $nessus::params::package_ensure,
+  String[1]                            $package_name    = $nessus::params::package_name,
   Optional[Stdlib::Host]               $proxy_host      = undef,
   Optional[Stdlib::Port::Unprivileged] $proxy_port      = undef,
-  Nessus::Service::Ensure    $service_ensure  = $nessus::params::service_ensure,
-  String[1]                  $service_name    = $nessus::params::service_name,
-  Boolean                    $service_enable  = $nessus::params::service_enable,
-  Boolean                    $service_manage  = $nessus::params::service_manage,
+  Nessus::Service::Ensure              $service_ensure  = $nessus::params::service_ensure,
+  String[1]                            $service_name    = $nessus::params::service_name,
+  Boolean                              $service_enable  = $nessus::params::service_enable,
+  Boolean                              $service_manage  = $nessus::params::service_manage,
 ) inherits nessus::params {
 
   anchor { 'nessus::begin': }
